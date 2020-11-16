89e06b518f14de46b3a99027ddd2476b4b23a54b
oliver.hu <540590515@qq.com>
2020-11-16 11:24:52 +0800

    MybatisForXml.iml

---
+++ mybatis/src/main/java/com/example/mybatisforxml/MybatisForXmlApplication.java
@@ -0,0 +1,16 @@
+package com.example.mybatisforxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author oliver.hu
+ */
+@SpringBootApplication
+public class MybatisForXmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisForXmlApplication.class, args);
+    }
+
+}
---
+++ mybatis/src/test/java/com/example/mybatisforxml/MybatisForXmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisforxml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisForXmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
