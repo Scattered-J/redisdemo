40a877f4c6fcf4a9218c76ee9c204176bb6e0901
oliver.hu <540590515@qq.com>
2020-11-18 10:20:20 +0800

    business test1

---
+++ businessmanage/src/main/java/com/example/businessmanage/BusinessmanageApplication.java
@@ -0,0 +1,16 @@
+package com.example.businessmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author oliver.hu
+ */
+@SpringBootApplication
+public class BusinessmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessmanageApplication.class, args);
+    }
+
+}
